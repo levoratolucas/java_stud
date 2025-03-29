2f4c9a188002fbbb761726602e532ed75e79f58b
levoratolucas <lucaslevorato@gmail.com>
2025-03-29 16:56:16 -0300

    relembrando consicionais etc...

--- projeto_1/src/App.java
+++ projeto_1/src/App.java
@@ -18,5 +18,24 @@ public class App {
         } else {
             System.out.println("Senha incorreta.");
         }
+
+        // ______________________________ pra concatenar variaveis dentro de box de texto ________________________
+
+        String mensagem = String.format("""
+                Olá, mundo!
+                Este é um Text Block.
+                Ele permite escrever %s textos com múltiplas linhas
+                sem precisar usar caracteres de escape ou quebras de linha manualmente ou concatenações.
+                """, senhaCorreta);
+        String nome = "João";
+        int aulas = 4;
+        String mensagem2 = """
+                Olá, %s!
+                Boas vindas ao curso de Java.
+                Teremos %d aulas para te mostrar o que é preciso para você dar o seu primeiro mergulho na linguagem!
+                """.formatted(nome, aulas);
+        // ________________________________________________________________________________________________________
+
+
     }
 }
