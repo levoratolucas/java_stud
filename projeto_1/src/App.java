433219be30d8e07d46f36f613a37c8734069e45f
levoratolucas <lucaslevorato@gmail.com>
2025-03-29 16:09:34 -0300

    olá mundo

---
+++ projeto_1/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a = 5;
+        int b = 10;
+        int soma = a + b;
+        String nome = "Lucas";
+        String ola = "Oi Tudo bem " + nome + "?";
+        System.out.println(ola);
+        System.out.println(soma);
+    }
+}
