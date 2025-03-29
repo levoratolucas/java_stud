8b4e9e439b8cffc7a6dee27875823c9f54eb7483
levoratolucas <lucaslevorato@gmail.com>
2025-03-29 16:41:15 -0300

    relembrando consicionais etc...

--- projeto_1/src/App.java
+++ projeto_1/src/App.java
@@ -2,34 +2,21 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // int a = 5;
-        // int b = 10;
-        // int soma = a + b;
-
-        // String nome ;
 
         Scanner scanner = new Scanner(System.in);
-
-        // System.out.println("Digite seu nome: ");
-
-        // nome = scanner.nextLine(); // levorato]
 
         Double pi = 3.146546;
 
         System.out.printf("nuemro teste %.4f \n\nqual a senha? \n\n", pi);
 
-        // String ola = "Oi Tudo bem " + nome + "?";
+        String senha = scanner.nextLine();
 
-        // System.out.println(ola);
-        // System.out.println(soma);
-        // System.out.println(nome);
+        String senhaCorreta = "12345";
 
-        String senha = scanner.nextLine();;
-        if (senha.equals("12345")) {
+        if (senha.equals(senhaCorreta)) {
             System.out.println("Acesso autorizado!");
         } else {
             System.out.println("Senha incorreta.");
         }
-
     }
 }
